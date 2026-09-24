a0ba8d12d0bad36fcafff70f746f0566e5688c44
Daniel Rodriguez III <DRIIIGistus@gmail.com>
2026-09-24 16:43:46 -0500

    Create schemas for snap growth datasets

---
+++ sql/01_create_datasets.sql
@@ -0,0 +1,4 @@
+CREATE SCHEMA IF NOT EXISTS `driiiportfolio.snap_growth_raw` OPTIONS(location='US');
+CREATE SCHEMA IF NOT EXISTS `driiiportfolio.snap_growth_transform` OPTIONS(location='US');
+CREATE SCHEMA IF NOT EXISTS `driiiportfolio.snap_growth_analytics` OPTIONS(location='US');
+CREATE SCHEMA IF NOT EXISTS `driiiportfolio.snap_growth_dashboard` OPTIONS(location='US');
